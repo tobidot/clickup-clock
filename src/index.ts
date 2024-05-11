f18db64d17b68c238d676a4f2431b74e4cb599c4
tobidot <object.name@live.de>
2024-05-11 15:39:37 +0200

    Various changes i dont remmber doing

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,13 @@ const chart = tgt.get_element_by_id("chart", SVGElement);
 new App(app, input, table, refresh, chart);
 
 try {
-    const token = sessionStorage.getItem("token");
+    const token = localStorage.getItem("token");
     if (token) {
         input.value = token;
         input.dispatchEvent(new Event("change"));
     }
     input.addEventListener("change", () => {
-        sessionStorage.setItem("token", input.value);
+        localStorage.setItem("token", input.value);
     });
 } catch (error) {
     console.error(error);
